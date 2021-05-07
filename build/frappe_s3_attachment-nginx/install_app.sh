40285f6d5ef6e4393cb8e8f8083c6703cd68e357
Pratik <pratik@improwised.com>
2021-05-07 14:26:50 +0530

    add rollup

--- build/frappe_s3_attachment-nginx/install_app.sh
+++ build/frappe_s3_attachment-nginx/install_app.sh
@@ -10,7 +10,7 @@ mkdir -p /home/frappe/frappe-bench/sites/assets
 cd /home/frappe/frappe-bench
 echo -e "frappe\n${APP_NAME}" > /home/frappe/frappe-bench/sites/apps.txt
 export  QT_QPA_PLATFORM=offscreen
-npm install -g chalk
+npm install -g chalk rollup
 
 mkdir -p apps
 cd apps
@@ -19,19 +19,19 @@ git clone --depth 1 ${APP_REPO} ${BRANCH} ${APP_NAME}
 
 echo "Install frappe NodeJS dependencies . . ."
 cd /home/frappe/frappe-bench/apps/frappe
-yarn &
-WAIT_YARN=$!
-## solve stuck while installing yarn
-while [[ $(ps -aux | grep ${WAIT_YARN} | grep -v grep | awk '{printf $2}') == ${WAIT_YARN} ]]; do echo "waiting for installation "; sleep $(( ( RANDOM % 10 ) + 1 )); done
+
 echo "Install ${APP_NAME} NodeJS dependencies . . ."
 cd /home/frappe/frappe-bench/apps/${APP_NAME}
 yarn
+
 echo "Build browser assets . . ."
 cd /home/frappe/frappe-bench/apps/frappe
 yarn production --app ${APP_NAME}
+
 echo "Install frappe NodeJS production dependencies . . ."
 cd /home/frappe/frappe-bench/apps/frappe
 yarn install --production=true
+
 echo "Install ${APP_NAME} NodeJS production dependencies . . ."
 cd /home/frappe/frappe-bench/apps/${APP_NAME}
 yarn install --production=true
