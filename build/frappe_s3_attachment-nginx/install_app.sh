70bbe5a0520064e550b47c152c1b70aa56513a07
pratikbalar <pratik@improwised.com>
2021-06-10 14:51:36 +0530

    fix: working custom app

--- build/frappe_s3_attachment-nginx/install_app.sh
+++ build/frappe_s3_attachment-nginx/install_app.sh
@@ -1,43 +1,38 @@
-#!/bin/bash -e
+#!/bin/bash -x
 
 APP_NAME=${1}
 APP_REPO=${2}
 APP_BRANCH=${3}
+FRAPPE_BRANCH=${4}
 
 [ "${APP_BRANCH}" ] && BRANCH="-b ${APP_BRANCH}"
 
 mkdir -p /home/frappe/frappe-bench/sites/assets
 cd /home/frappe/frappe-bench
 echo -e "frappe\n${APP_NAME}" > /home/frappe/frappe-bench/sites/apps.txt
-export  QT_QPA_PLATFORM=offscreen
 
 mkdir -p apps
 cd apps
-git clone --depth 1 https://github.com/frappe/frappe ${BRANCH}
+[ ! -d frappe ] && git clone --depth 1 https://github.com/frappe/frappe -b ${FRAPPE_BRANCH}
 git clone --depth 1 ${APP_REPO} ${BRANCH} ${APP_NAME}
 
 echo "Install frappe NodeJS dependencies . . ."
 cd /home/frappe/frappe-bench/apps/frappe
 yarn
-
 echo "Install ${APP_NAME} NodeJS dependencies . . ."
 cd /home/frappe/frappe-bench/apps/${APP_NAME}
 yarn
-
 echo "Build browser assets . . ."
 cd /home/frappe/frappe-bench/apps/frappe
 yarn production --app ${APP_NAME}
-
 echo "Install frappe NodeJS production dependencies . . ."
 cd /home/frappe/frappe-bench/apps/frappe
 yarn install --production=true
-
 echo "Install ${APP_NAME} NodeJS production dependencies . . ."
 cd /home/frappe/frappe-bench/apps/${APP_NAME}
 yarn install --production=true
 
 mkdir -p /home/frappe/frappe-bench/sites/assets/${APP_NAME}
-
 if [[ ${APP_NAME} == *"frappe_s3_attachment"* ]]; then
   cp -R /home/frappe/frappe-bench/apps/${APP_NAME}/${APP_NAME}/* /home/frappe/frappe-bench/sites/assets/${APP_NAME}
 else
